91487ea57fa048537107fc04967edf78b79f29c4
cf9874 <cf9874@gmail.com>
2023-01-20 20:42:41 +0900

    Design : item 상세페이지 디자인중

--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import { list } from "@/src/common";
+import axios from "axios";
+import Image from "next/image";
 
 export default function List() {
   const router = useRouter();
@@ -12,15 +14,38 @@ export default function List() {
   return (
     <>
       <div className="listBox">
-        <div>{item?.id}</div>
-        <div>{item?.name}</div>
-        <div>{item?.price}</div>
+        <h1>{item.name}</h1>
+        <div className="image">
+          <img src="/images/pork_neck.jpg" alt="돼지목살" />
+        </div>
+        <div className="contentsBox">
+          <div>오늘자가격 : {item.price}원</div>
+          <div>어제보다 몇 %</div>
+          <div>지난달 보다 몇 %</div>
+        </div>
+        <div className="contentsBox">
+          <div>1</div>
+          <div>2</div>
+          <div>2</div>
+        </div>
+        <div>{item.price}</div>
       </div>
       <style jsx>
         {`
           .listBox {
+            margin: 0 auto;
             border: 1px solid red;
-            width: 300px;
+            width: 100%;
+          }
+          .image > img {
+            width: 20vw;
+            height: 20vw;
+            margin: 0 auto;
+          }
+          .contentsBox {
+            margin-top: 50px;
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
           }
         `}
       </style>
